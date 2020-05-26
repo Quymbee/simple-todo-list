7129b27b62d401a8f785edcc05c570eebe973373
Quymbee Chen <quymbee@mattchen.com>
2020-05-26 13:03:49 -0400

    finished todo tutorial

---
+++ todo/src/App.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Todos from './components/Todos';
+import Header from './components/layout/Header'
+import AddTodo from './components/AddTodo'
+import About from './components/pages/About'
+//import {v4 as uuid} from 'uuid'
+import axios from 'axios'
+
+import './App.css';
+
+class App extends Component {
+  state = {
+    todos: []
+  }  
+  componentDidMount(){
+    axios.get('http://jsonplaceholder.typicode.com/todos?_limit=10')
+      .then(res => this.setState({todos: res.data}))
+  }
+
+  //toggle complete checkbox
+  markComplete = (id) => {
+    this.setState({ todos: this.state.todos.map(todo => {
+      if(todo.id === id){
+        todo.completed = !todo.completed
+      }
+      return todo;
+    })})
+  }
+
+  //delete todo
+  delTodo = (id) => {
+    axios.delete(`http://jsonplaceholder.typicode.com/todos/${id}`)
+      .then(res => this.setState({ todos: [...this.state.todos.filter
+        (todo => todo.id !== id)] }));
+  }
+
+  addTodo = (title) => {
+    axios.post('http://jsonplaceholder.typicode.com/todos', {
+      title,
+      completed: false
+    }).then(res => this.setState({ todos: [...this.state.todos, res.data]})); 
+  }
+
+  render(){
+    return (
+      <Router>
+        <div className="App">
+          <div className='container'>
+            <Header />
+            <Route exact path='/' render={ props => (
+              <React.Fragment>
+                <AddTodo addTodo={this.addTodo}/>
+                <Todos todos={this.state.todos} markComplete={this.markComplete}
+                delTodo={this.delTodo}/>
+              </React.Fragment>
+            )}/>
+
+            <Route path='/about' component={ About } />
+
+          </div>
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
---
+++ todo/src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function About() {
+    return (
+        <React.Fragment>
+            <h1> About </h1>
+            <p> this is the Todolist app. </p>
+        </React.Fragment>
+    )
+}
+
+export default About;
